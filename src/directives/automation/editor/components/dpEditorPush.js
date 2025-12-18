b09c81c1e5933589e431220dd4283527d9c04b9a
cketibian <cketibian@makingsense.com>
2025-12-18 16:30:54 -0300

    fix: test push

--- src/directives/automation/editor/components/dpEditorPush.js
+++ src/directives/automation/editor/components/dpEditorPush.js
@@ -50,8 +50,8 @@
         }
         if(domains.length > 0) {
           const REGEX_URL = /^https:\/\/.+\..+/;
-          const firstWithValidIcon = domains.find(d => d.icon && REGEX_URL.test(d.icon));
-          return firstWithValidIcon || defaultIconPath;
+          const firstWithValidIcon = domains.find(d => d.WebSiteLogoUrl && REGEX_URL.test(d.WebSiteLogoUrl));
+          return firstWithValidIcon.WebSiteLogoUrl || defaultIconPath;
         }
         return defaultIconPath
       }
